6c1e99366cec5422d28b285f930a922790800bbb
vindipop <vindipopardinoto22@gmail.com>
2023-10-18 23:27:49 +0700

    test commit

--- app/src/main/java/com/example/kantongku/MainActivity.kt
+++ app/src/main/java/com/example/kantongku/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KantongkuTheme {
-                // A surface container using the 'background' color from the theme
+                // testing A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
